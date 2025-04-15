807077fba4f38658c4eafe182d86e54a262dc14e
OlhaFedorets <fedoretsolha@gmail.com>
2025-04-15 14:46:31 +0300

    change app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import { useAppSelector } from "@/common/hooks"
 import { getTheme } from "@/common/theme"
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
+import { ErrorSnackbars } from "@/common/components/ErrorSnackbar/ErrorSnackbar.tsx"
 
 export const App = () => {
   const themeMode = useAppSelector(selectThemeMode)
@@ -18,6 +19,7 @@ export const App = () => {
         <CssBaseline />
         <Header />
         <Main />
+        <ErrorSnackbars/>
       </div>
     </ThemeProvider>
   )
